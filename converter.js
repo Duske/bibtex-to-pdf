3939d88ee49e11d3e8f5acf3a75d5361a2a4658f
Duske <dustin.duske@gmail.com>
2015-04-24 12:44:55 +0200

    basic download if url points directly to a pdf file

--- converter.js
+++ converter.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var wkhtmltopdf = require('wkhtmltopdf');
 var Q = require('q');
+var http = require('http');
+var fs = require('fs');
 
 function writePdf(url, filename) {
   var deferred = Q.defer();
   wkhtmltopdf(url,
     {
       output: 'pdf/'+filename,
-      javascriptDelay: 800
+      javascriptDelay: 2000
     },
     function (code, signal) {
       //Ignore code QFont::setPixelSize: Pixel size <= 0 (0)
@@ -28,22 +30,40 @@ function cleanSlashes(text) {
   return text.replace(/\//g, "-");
 }
 
+function composeFileName(nameParts, filetype) {
+  return (cleanSlashes(nameParts.reduce(function(prev, current) {
+    return prev+'-'+current;
+  })
+  +'.'+filetype));
+}
+
 
 module.exports = {
 
   convertBibtexJsonToPdf: function(urls) {
     var workCount = 0;
     var max = urls.length;
+    var file;
+    var tempFileName;
     for (var i = 0; i < max; i++) {
       if(!isDownloadableContent(urls[i].url)) {
         workCount++;
-        console.log(urls[i].url+ ' cannot be converted because of an incompatible filetype.')
-        fs.appendFile('error-log.txt', "This resource could not be converted. Incompatible filetype:"+urls[i].url+"\n", function (err) {
-
+        tempFileName = 'pdf/' + composeFileName([urls[i].author, urls[i].title], 'pdf');
+        file = fs.createWriteStream(tempFileName);
+        var request = http.get(urls[i].url, function(response) {
+          response.pipe(file);
+          file.on('finish', function() {
+            file.close(function() {
+              console.log("Successfully downloaded "+tempFileName);
+            });
+          });
+        }).on('error', function(err) { // Handle errors
+          fs.unlink(tempFileName); // Delete the file async. (But we don't check the result)
+          console.log("An error occured with "+tempFileName);
         });
       }
       else {
-        writePdf(urls[i].url, cleanSlashes(urls[i].author)+'-'+cleanSlashes(urls[i].title)+'.pdf')
+        writePdf(urls[i].url, composeFileName([urls[i].author, urls[i].title], 'pdf'))
         .then(function(filename) {
           console.log("Successfully downloaded "+filename);
         })
--- extractor.js
+++ extractor.js
@@ -1,16 +1,4 @@
-function isValidUrl(str) {
-  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
-  '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
-  '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
-  '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
-  '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
-  '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
-  if(!pattern.test(str)) {
-    return false;
-  } else {
-    return true;
-  }
-}
+var validUrl = require('valid-url');
 
 module.exports = {
 
@@ -25,7 +13,7 @@ module.exports = {
           for (var prop in sourceitem) {
              if (sourceitem.hasOwnProperty(prop)) {
                 if (prop === "URL") {
-                  if (!isValidUrl(encodeURI(sourceitem[prop]))) {
+                  if (!validUrl.is_web_uri(encodeURI(sourceitem[prop]))) {
                     console.log("Invalid URL at item "+sourceitem[prop]);
                     break;
                   }
