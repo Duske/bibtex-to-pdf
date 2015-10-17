be4e5bd04d13d0cffbfef6a44fc5ad76819d141e
Dustin Chabrowski <dustin.duske@gmail.com>
2015-10-17 17:07:41 +0200

    adjusted tests to new code structure

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
 var chai = require('chai');
 var assert = chai.assert;
+var should = require('chai').should();
 var converter = require('../converter');
+var filenameUtility = require('../filename-utility');
 var extractor = require('../extractor');
 
-describe('converter.js', function(){
+describe('filename-utility.js', function(){
   describe('composeFileName', function(){
     it('should compose a filename', function(){
-      var result = converter.composeFileName(['a', 'b'], 'pdf');
+      var result = filenameUtility.composeFileName(['a', 'b'], 'pdf');
       assert.equal('a-b.pdf', result);
     });
   });
@@ -23,9 +25,11 @@ describe('extractor.js', function(){
       noUrlProperty: {
         'AUTHOR': 'testauthor2',
         'TITLE': 'testtitle2'
-      },
+      }
+    };
+    var inValidUrlObject = {
       inValidUrl: {
-        'URL': 'http:/testcom',
+        'URL': 'sd:/testcom',
         'AUTHOR': 'testauthor',
         'TITLE': 'testtitle'
       }
@@ -40,6 +44,11 @@ describe('extractor.js', function(){
     it('should extract the sources', function(){
       var result = extractor.getExtractedSourceList(bibtextObject);
       assert.deepEqual(bibtextArray, result);
+    });
+    it('should throw an error if URL is invalid', function() {
+      assert.throw(function() {
+        extractor.getExtractedSourceList(inValidUrlObject)
+      }, inValidUrlObject.inValidUrl['URL'] + ' is not a valid URL');
     });
     it('should return an empty array if no object is supplied', function(){
       var result1 = extractor.getExtractedSourceList(null);
