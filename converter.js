49c4925548dbd7920bcd19c6727dd015410889e1
Dustin Chabrowski <dustin.duske@gmail.com>
2016-01-29 22:42:22 +0100

    [TASK] add simple function documentation

--- converter.js
+++ converter.js
@@ -12,6 +12,12 @@ const errorMessage = chalk.bold.white.bgRed;
 const infoMessage = chalk.blue.bgWhite;
 const successMessage = chalk.black.bold.bgGreen;
 
+/**
+ * Downloads a webpage and converts it to a pdf file
+ * @param  {String} url      URL of the target page
+ * @param  {String} filename filename of the pdf file
+ * @return {Promise}
+ */
 function convertWebpageToPdf(url, filename) {
   const output = path.resolve(filePath, filename);
   return wkhtmltopdf(url, {
@@ -27,6 +33,12 @@ function convertWebpageToPdf(url, filename) {
   });
 }
 
+/**
+ * Downloads a file from a webpage and writes it to disk
+ * @param  {String} url      URL of the target page
+ * @param  {String} filename filename of the file
+ * @return {Promise}
+ */
 function downloadFile(url, filename) {
   const output = path.resolve(filePath, filename);
   return request(url, {
@@ -46,6 +58,11 @@ function downloadFile(url, filename) {
 
 module.exports = {
 
+  /**
+   * Processes an array of urlitems and downloads and convert the containg URLs to pdf files
+   * @param  {array} urlItems List of urlItems containg url, author and TITLE
+   * @return {void}          [description]
+   */
   convertBibtexJsonToPdf: function(urlItems) {
     var tempFileName;
     var that = this;
