f6081e9479b9a17cee9ea933e1b9fcd6c8508d9a
Dustin Chabrowski <dustin.duske@gmail.com>
2016-01-29 21:53:15 +0100

    [TASK] promisify wkhtmltopdf

--- converter.js
+++ converter.js
@@ -1,29 +1,31 @@
-var wkhtmltopdf = require('wkhtmltopdf');
+var wkhtmltopdfRaw = require('wkhtmltopdf');
 var Promise = require('bluebird');
 var fs = Promise.promisifyAll(require('fs'));
+var path = require('path');
 var request = require('request');
 var filenameUtility = require('./filename-utility');
 const filePath = 'pdf/';
+const wkhtmltopdf = Promise.promisify(wkhtmltopdfRaw);
 
-function writePdf(url, filename) {
-  return new Promise(function(resolve, reject) {
-    wkhtmltopdf(url,
-      {
-        output: filePath + filename,
-        javascriptDelay: 2000
-      },
-      function (code, signal) {
-        //Ignore code QFont::setPixelSize: Pixel size <= 0 (0)
-        if(signal !== 0 && code && code.message !== 'QFont::setPixelSize: Pixel size <= 0 (0)') {
-          reject(code);
-        } else {
-          resolve(filename);
-        }
-      });
+function convertWebpageToPdf(url, filename) {
+  const output = path.resolve(filePath, filename);
+  return wkhtmltopdf(url, {
+    output: path.resolve(filePath, filename),
+    javascriptDelay: 2000
+  })
+  .then(function() {
+    return output;
+  })
+  .catch(function(error) {
+    console.log(`An error occured while converting URL ${url}`);
+    throw Error(error);
   });
 }
 
 function downloadFile(url, filename) {
+  // return request(url, {
+  //   encoding: null
+  // }).then
   return new Promise(function(resolve) {
     request(url)
       .pipe(fs.createWriteStream(filePath + filename))
@@ -48,7 +50,10 @@ module.exports = {
         return downloadFile(urlItems[index].url, tempFileName);
       } else {
         console.log('Start converting ' + urlItems[index].title)
-        return writePdf(urlItems[index].url, tempFileName);
+        return convertWebpageToPdf(urlItems[index].url, tempFileName)
+        .then(function(filename) {
+          console.log(`Successfully downloaded ${filename}`);
+        });
       }
     });
 
@@ -56,8 +61,9 @@ module.exports = {
       .then(function() {
         console.log('Successfully processed all items');
       })
-      .catch(function() {
+      .catch(function(error) {
         console.log('There have been errors, please see error-log.txt');
+        console.log(error);
       });
   }
 };
